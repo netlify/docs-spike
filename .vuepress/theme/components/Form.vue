abfb2c113c3c4a922f32345689331649922d2b29
lunaceee <lunaceee@gmail.com>
2019-03-19 20:36:53 -0700

    test form submit

--- .vuepress/theme/components/Form.vue
+++ .vuepress/theme/components/Form.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="feedback">
     <div class="helptext">
-      <h3>Did you find this doc useful?</h3>
+      <h3>Did you find this doc useful??</h3>
       <p>Your feedback helps us improve these docs</p>
     </div>
     <div class="feedback-icons">
-      <form name="upvote" method="POST" netlify id="upvote-form" @submit.prevent="handleSubmit">
+      <form name="upvote" method="POST" netlify id="upvote-form">
         <input type="hidden" name="form-name" value="upvote">
-        <img src="./illo-thumb-up.svg" alt v-on:click="votes">
+        <img src="./illo-thumb-up.svg" alt v-on:click="upvote">
       </form>
       <form name="downvote" method="POST" netlify id="downvote-form">
         <input type="hidden" name="form-name" value="downvote">
-        <img src="./illo-thumb-down.svg" alt v-on:click="votes">
+        <img src="./illo-thumb-down.svg" alt v-on:click="downvote">
       </form>
     </div>
   </div>
@@ -48,10 +48,18 @@ export default {
     };
   },
   methods: {
-    votes: function(e) {
-      this.upvoteCount = e.target.value;
-      console.log(e.target.value);
-    }
+    upvote: function(e) {
+      fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: {
+          "form-name": "upvote"
+        }
+      });
+    },
+    downvote: function(e) {}
   }
 };
 </script>
